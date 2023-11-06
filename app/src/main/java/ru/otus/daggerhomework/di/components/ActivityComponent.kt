55693459fd8da785ebd0b17435b8589c4235c975
mvrlrd <mvrlrd@yandex.ru>
2023-11-06 21:48:04 +0300

    add ActivityComponent

---
+++ app/src/main/java/ru/otus/daggerhomework/di/components/ActivityComponent.kt
@@ -0,0 +1,47 @@
+package ru.otus.daggerhomework.di.components
+
+import android.content.Context
+import dagger.BindsInstance
+import dagger.Component
+import ru.otus.daggerhomework.color_manager.ColorEmitter
+import ru.otus.daggerhomework.color_manager.ColorObserver
+import ru.otus.daggerhomework.MainActivity
+import ru.otus.daggerhomework.di.ActivityScope
+import ru.otus.daggerhomework.di.modules.ActivityModule
+import javax.inject.Named
+
+@ActivityScope
+@Component(
+    modules = [ActivityModule::class],
+    dependencies = [ApplicationComponent::class]
+)
+interface ActivityComponent {
+
+    fun inject(mainActivity: MainActivity)
+
+    @Named("applicationContext")
+    fun provideAppContext(): Context
+
+    @Named("mainActivityContext")
+    fun provideActivityContext(): Context
+
+    fun provideColorEmitter(): ColorEmitter
+    fun provideColorObserver(): ColorObserver
+
+    @Component.Factory
+    interface Factory {
+        fun create(
+            applicationComponent: ApplicationComponent,
+            @BindsInstance @Named("mainActivityContext") activityContext: Context
+        ): ActivityComponent
+    }
+
+    companion object {
+        fun getActivityComponent(
+            applicationComponent: ApplicationComponent,
+            context: Context
+        ): ActivityComponent {
+            return DaggerActivityComponent.factory().create(applicationComponent, context)
+        }
+    }
+}
