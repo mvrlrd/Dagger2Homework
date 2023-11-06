ef4802c6facd0775dbe9c30578df7207d2a06d4d
mvrlrd <mvrlrd@yandex.ru>
2023-11-06 20:36:05 +0300

    add scopes

---
+++ app/src/main/java/ru/otus/daggerhomework/scopes.kt
@@ -0,0 +1,15 @@
+package ru.otus.daggerhomework
+
+import javax.inject.Scope
+
+@Scope
+annotation class ApplicationScope
+
+@Scope
+annotation class ActivityScope
+
+@Scope
+annotation class ProducerScope
+
+@Scope
+annotation class ReceiverScope
