a957888759731148a101e4fc79734ff7c456b282
mvrlrd <mvrlrd@yandex.ru>
2023-11-06 21:48:23 +0300

    init activityComponent

--- app/src/main/java/ru/otus/daggerhomework/MainActivity.kt
+++ app/src/main/java/ru/otus/daggerhomework/MainActivity.kt
@@ -2,11 +2,20 @@ package ru.otus.daggerhomework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.otus.daggerhomework.di.components.ActivityComponent
 
 class MainActivity : AppCompatActivity() {
 
+    val activityComponent: ActivityComponent by lazy {
+        ActivityComponent.getActivityComponent(
+            (application as App).getApplicationComponent(),
+            this
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        activityComponent.inject(this)
         setContentView(R.layout.activity_main)
     }
 }
